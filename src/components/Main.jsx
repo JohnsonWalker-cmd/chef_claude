85dd07d76f0c100c6a37584a023997cba56f3d96
JohnsonWalker-cmd <sakajohnson1@gmail.com>
2025-10-15 19:08:32 +0000

    Add conditional rendering to show recipe when ingredients are more than 3 and also updated form to get formData directly from the addIngredient function

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 export default function Main(){
     const [ingredients , setIngredients] = useState([])
 
-    const ingredient_item = ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)
+    const ingredientsListItems = ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)
 
     function addIngredient(formData){
         const newIngredient = formData.get("ingredient")
@@ -20,9 +20,17 @@ export default function Main(){
                 />
                 <button>Add ingredient</button>
             </form>
-            <ol>
-                {ingredient_item}
-            </ol>
+            {ingredients.length > 0 ? <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                {ingredients.length > 3 && <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>} 
+            </section> : null}
         </main>
     )
 }
