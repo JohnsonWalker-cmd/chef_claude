90206a865a29698136255ef404f4bf57a2016f8d
JohnsonWalker-cmd <sakajohnson1@gmail.com>
2025-10-15 15:31:09 +0000

    update form using the action attribute in modern react 19

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,15 +5,13 @@ export default function Main(){
 
     const ingredient_item = ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)
 
-    function handleSubmit(event){
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
+    function addIngredient(formData){
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => [...prevIngredients , newIngredient])
     }
     return (
         <main>
-            <form className="add-ingredient-form" onSubmit={handleSubmit}>
+            <form className="add-ingredient-form" action={addIngredient}>
                 <input
                 type="text" 
                 placeholder="e.g. oregano"
